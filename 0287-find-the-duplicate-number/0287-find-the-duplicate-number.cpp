860bebea967da10ffbdb9fe84471d44d67cbb3d1
Vatsal Agarwal <80644981+vatsal-agarwal-20@users.noreply.github.com>
2022-11-20 12:44:34 +0530

    Time: 119 ms (94.29%), Space: 61.3 MB (62.47%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,21 +1,8 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        int tortoise = nums[0];
-        int hare = nums[0];
-
-        do {
-            tortoise = nums[tortoise];
-            hare = nums[nums[hare]];
-        } while (tortoise != hare);
-        
-        // Find the "entrance" to the cycle.
-        tortoise = nums[0];
-        while (tortoise != hare) {
-            tortoise = nums[tortoise];
-            hare = nums[hare];
-        }
-        
-        return hare;
+        while (nums[0] != nums[nums[0]])
+            swap(nums[0], nums[nums[0]]);
+        return nums[0];
     }
 };
