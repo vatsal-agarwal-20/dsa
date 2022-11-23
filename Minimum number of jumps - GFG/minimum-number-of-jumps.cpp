8172db68e15a7b04bf9e6887f4f96c66b7ae2bf8
Vatsal Agarwal <80644981+vatsal-agarwal-20@users.noreply.github.com>
2022-11-23 16:39:44 +0530

    Added solution - LeetHub

--- Minimum number of jumps - GFG/minimum-number-of-jumps.cpp
+++ Minimum number of jumps - GFG/minimum-number-of-jumps.cpp
@@ -14,8 +14,8 @@ class Solution{
         int des=0;
         int jumps=0;
         
-        if(n==1) return 0;
-        if(arr[0]==0) return -1;
+        // if(n==1) return 0;
+        // if(arr[0]==0) return -1;
         
         for(int i=0;i<n-1;i++)
         {
@@ -27,7 +27,7 @@ class Solution{
                 jumps++;
             }
         }
-        if(des < n-1) return -1;
+        if(pos < n-1) return -1;
 
         return jumps;
     }
