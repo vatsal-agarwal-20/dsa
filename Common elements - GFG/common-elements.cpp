bf479795fa815a9b8356ccd0d45aef47c5f3be1d
Vatsal Agarwal <80644981+vatsal-agarwal-20@users.noreply.github.com>
2022-11-24 00:11:46 +0530

    Added solution - LeetHub

--- Common elements - GFG/common-elements.cpp
+++ Common elements - GFG/common-elements.cpp
@@ -14,38 +14,24 @@ class Solution
             
             int i=0,j=0,k=0;
             
-            while(i<n1 && j<n2 && k<n3)
+            while(i<n1 && j< n2 && k< n3)
             {
-                if(A[i] < B[j])
+                if(A[i]==B[j] && B[j]==C[k])
                 {
+                    if(ans.size()==0 || A[i] != ans.back())
+                    ans.push_back(A[i]);
+                    
+                    i++; j++; k++;
+                }
+                else if(A[i]<B[j])
                     i++;
-                }
-                else if(A[i] == B[j])
-                {
-                    if(A[i] == C[k])
-                    {
-                        if(ans.size()==0 || A[i] != ans.back())
-                        {
-                            ans.push_back(A[i]);
-                        }
-                        i++;
-                    }
-                    else if(A[i] < C[k])
-                    {
-                        i++;
-                    }
-                    else
-                    {
-                        k++;
-                    }
-                }
-                else
-                {
+                else if(B[j]<C[k])
                     j++;
-                }
+                else
+                    k++;
             }
             return ans;
-        }
+            }
 
 };
 
