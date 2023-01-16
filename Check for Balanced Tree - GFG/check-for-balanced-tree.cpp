dec03d14dd0ec3c618bf457dc18c83b5e76d0274
Vatsal Agarwal <80644981+vatsal-agarwal-20@users.noreply.github.com>
2023-01-16 23:14:35 +0530

    Added solution - LeetHub

--- Check for Balanced Tree - GFG/check-for-balanced-tree.cpp
+++ Check for Balanced Tree - GFG/check-for-balanced-tree.cpp
@@ -102,29 +102,13 @@ struct Node
  */
 
 class Solution{
-    pair<bool,int> isBalancedFast(Node* root)
+    int height(Node* root)
     {
-        if(root==NULL)
-        {
-            pair<bool,int> p=make_pair(true,0);
-            return p;
-        }
-        pair<bool,int> left= isBalancedFast(root->left);
-        pair<bool,int> right= isBalancedFast(root->right);
+        if(root==NULL) return 0;
         
-        bool leftAns=left.first;
-        bool rightAns=right.first;
-        
-        bool diff = abs(left.second - right.second) <=1;
-        
-        pair<bool,int> ans;
-        ans.second=max(left.second,right.second)+1;
-        
-        if(leftAns && rightAns && diff)
-        ans.first=true;
-        
-        else
-        ans.first=false;
+        int left=height(root->left);
+        int right=height(root->right);
+        int ans=max(left,right)+1;
         
         return ans;
     }
@@ -133,7 +117,15 @@ class Solution{
     bool isBalanced(Node *root)
     {
         //  Your Code here
-        return isBalancedFast(root).first;
+        if(root==NULL) return true;
+        
+        bool left=isBalanced(root->left);
+        bool right=isBalanced(root->right);
+        
+        bool diff= abs(height(root->left) - height(root->right)) <=1;
+        
+        if(left && right && diff) return true;
+        else return false;
     }
 };
 
