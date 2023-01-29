22d6d615103422331445adc5e410b11da563032c
Vatsal Agarwal <80644981+vatsal-agarwal-20@users.noreply.github.com>
2023-01-30 01:06:00 +0530

    Added solution - LeetHub

--- Largest BST - GFG/largest-bst.cpp
+++ Largest BST - GFG/largest-bst.cpp
@@ -98,50 +98,49 @@ struct Node {
         left = right = NULL;
     }
 };*/
-class info{
-  public:
-  int maxi;
-  int mini;
-  bool isBST;
-  int size;
-};
-
-
-class Solution{
-    info solve(Node* root,int &ans)
+class info
 {
-    if(root==NULL)
-    {
-        return {INT_MIN,INT_MAX,true,0};
-    }
-    
-    info left=solve(root->left,ans);
-    info right=solve(root->right,ans);
-    
-    info currNode;
-    
-    currNode.size=left.size+right.size+1;
-    currNode.maxi=max(root->data,right.maxi);
-    currNode.mini=min(root->data,left.mini);
-    
-    if(left.isBST && right.isBST &&
-    (root->data > left.maxi && root->data < right.mini))
-    {
-        currNode.isBST=true;
-    }
-    else{
-        currNode.isBST=false;
-    }
-    
-    //Answer update
-    if(currNode.isBST)
-    {
-        ans=max(ans,currNode.size);
-    }
-    return currNode;
-}
-    
     public:
+    int maxi;
+    int mini;
+    bool isBST;
+    int size;
+};
+class Solution{
+    public:
+    info solve(Node* root, int &ans)
+    {
+        if(root==NULL)
+        {
+            return {INT_MIN,INT_MAX,true,0};
+        }
+        
+        info left=solve(root->left,ans);
+        info right=solve(root->right,ans);
+        
+        info currNode;
+        
+        currNode.size=left.size + right.size + 1;
+        currNode.maxi= max(root->data,right.maxi);
+        currNode.mini= min(root->data,left.mini);
+        
+        if(left.isBST && right.isBST && root->data < right.mini && root->data > left.maxi)
+        {
+            currNode.isBST=true;
+        }
+        else
+        {
+            currNode.isBST=false;
+        }
+        
+        if(currNode.isBST)
+        {
+            ans= max(ans, currNode.size);
+        }
+        
+        return currNode;
+        
+    }
     /*You are required to complete this method */
     // Return the size of the largest sub-tree which is also a BST
     int largestBst(Node *root)
